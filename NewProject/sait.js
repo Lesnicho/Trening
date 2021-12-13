33e0a5687d2d51df4cf446e518b6c53838d5c875
Lesnicho <lesnik.artemij@bk.ru>
2021-12-13 14:00:41 +0300

    JS Work-Done

---
+++ NewProject/sait.js
@@ -0,0 +1,11 @@
+let input1 = document.querySelector('.inp1');
+let input2 = document.querySelector('.inp2');
+
+let button = document.querySelector('.btn')
+
+let p = document.querySelector('.p')
+
+button.onclick = () =>{
+    p.innerHTML = Number(input1.value) + Number(input2.value)
+    
+}
