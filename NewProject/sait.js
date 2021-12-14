c0556caff1ba19aa41f17342c7b140937feeea46
Lesnicho <lesnik.artemij@bk.ru>
2021-12-14 15:14:15 +0300

    визуальное улудшение

--- NewProject/sait.js
+++ NewProject/sait.js
@@ -1,8 +1,6 @@
 let input1 = document.querySelector('.inp1');
 let input2 = document.querySelector('.inp2');
-
-let button = document.querySelector('.btn')
-
+let button = document.querySelector('.btn');
 let p = document.querySelector('.p')
 
 button.onclick = () =>{
